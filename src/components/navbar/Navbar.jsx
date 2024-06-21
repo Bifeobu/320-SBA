a90b3a7c8a3b3354a3b982fcc373917836acdddb
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-21 01:38:04 -0400

    navbar css and jsx modification

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,25 +4,16 @@ function Navbar() {
 
     return(
         <div class="navbar">
-            <a href='home'>Home</a>
-            <div class="dropdownC">
-                <button class="dropbtnC">Character
-                    <i class="fa fa-caret down"></i>
-                </button>
-                <div class="dropdown-contentC"></div>
-            </div>
-            
-            
-            
-            <div class="dropdownL"></div>
-            <div class="dropdownE"></div>
+            <nav>
+                <a href='home'>Home</a>
+                <a href='character'>Character</a>
+                <a href='location'>Location</a>
+                <a href='episode'>Episode</a>
+            </nav>
 
-
-
-
-            <a href='character'>Character</a>
+            {/* <a href='character'>Character</a>
             <a href='location'>Location</a>
-            <a href='episode'>Episode</a>
+            <a href='episode'>Episode</a> */}
         </div>
     );
 } 
