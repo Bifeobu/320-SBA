3a45aecc4b1e13e99392b8293208af470ec9acf8
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-20 23:28:04 -0400

    navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './Navbar.css'
+
+function Navbar() {
+
+    return(
+        <div class="navbar">
+            <a href='home'>Home</a>
+            <a href='character'>Character</a>
+            <a href='location'>Location</a>
+            <a href='episode'>Episode</a>
+        </div>
+    );
+} 
+
+export default Navbar
