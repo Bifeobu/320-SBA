47798e8d049c0a157b350d52974b0a52bca48819
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-20 23:54:09 -0400

    navbar mod

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,6 +5,21 @@ function Navbar() {
     return(
         <div class="navbar">
             <a href='home'>Home</a>
+            <div class="dropdownC">
+                <button class="dropbtnC">Character
+                    <i class="fa fa-caret down"></i>
+                </button>
+                <div class="dropdown-contentC"></div>
+            </div>
+            
+            
+            
+            <div class="dropdownL"></div>
+            <div class="dropdownE"></div>
+
+
+
+
             <a href='character'>Character</a>
             <a href='location'>Location</a>
             <a href='episode'>Episode</a>
