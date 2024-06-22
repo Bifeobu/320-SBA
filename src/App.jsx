f826a07fc26629840341aec2452f03766605ff74
Beverly Ifeobu <beverlyifeobu@gmail.com>
2024-06-22 00:45:01 -0400

    character link

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 // import Header from './components/Header';
 // import Navbar from './components/navbar/Navbar';
 import Home from './components/navbar/navlinks/Home';
+import Character from './components/navbar/navlinks/Character';
 import './App.css'
 
 
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/character" element={<Character />}/>
         </Routes>
       </div>
     </>
--- src/components/navbar/navlinks/Character.jsx
+++ src/components/navbar/navlinks/Character.jsx
@@ -3,6 +3,32 @@ import Navbar from '../Navbar';
 
 function Character() {
 
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://rickandmortyapi.com/api/character')
+        .then(response => response.json())
+        .then(data => setData(data))
+        .catch(error => console.log("Error in datafetching", error));
+    }, []);
+
+    //Having trouble displaying data from api onto the webpage, I see how to see them in the console but cannot figure out how to see it on the webpage
+    console.log(data)
+    const dataResults = console.log(data.results)
+    console.log(data.results[0])
+
+    return (
+        <div>
+            <h1>Rick and Morty Characters</h1>
+            <h2>{dataResults}</h2>
+
+            {/* <ul>
+                {data.map((results) => {
+                    <li key={results.id}>{results.name}</li>
+                })}
+            </ul> */}
+        </div>
+    )
 }
 
 export default Character
